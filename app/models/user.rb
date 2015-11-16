a5002875ad40ec4ce5292728fd465f24052e93b1
David McDonald <dmcdonald@sellbrite.com>
2015-11-15 21:26:31 -0800

    just a comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  # to find out more: https://github.com/plataformatec/devise
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
